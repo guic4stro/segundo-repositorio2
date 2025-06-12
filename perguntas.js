fa9c08add47ba152b4c5560a38cb56a6adea368e
guic4stro <guilherme.castro05@escola.pr.gov.br>
2025-06-11 21:09:36 -0300

    Aula 11/06

--- perguntas.js
+++ perguntas.js
@@ -1,29 +1,89 @@
 criaCartao(
-    'Programação', 
-    'O que é Python?', 
-    'O Python é uma linguagem de programação'
-)
+  'HTML',
+  'O que é HTML?',
+  'HTML é a linguagem de marcação usada para estruturar o conteúdo de páginas web.'
+);
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
-)
+  'HTML',
+  'Para que serve a tag <p>?',
+  'A tag <p> é usada para definir parágrafos de texto.'
+);
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
-)
-
-criaCartao (
-    'Programação',
-    'O que é JavaScript?',
-    'JavaScript é uma linguagem de programação'
-)
+  'HTML',
+  'Qual a função da tag <a>?',
+  'A tag <a> é usada para criar links entre páginas ou seções.'
+);
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz OI em inglês?',
-    'Oi em inglês é HI (rai)'
-)
+  'HTML',
+  'O que a tag <img> faz?',
+  'A tag <img> insere imagens na página HTML.'
+);
+
+criaCartao(
+  'HTML',
+  'Qual a diferença entre <ul> e <ol>?',
+  '<ul> cria listas não ordenadas, enquanto <ol> cria listas ordenadas.'
+);
+
+criaCartao(
+  'CSS',
+  'O que é CSS?',
+  'CSS é uma linguagem de estilo usada para definir a aparência de elementos HTML.'
+);
+
+criaCartao(
+  'CSS',
+  'Como mudar a cor do texto com CSS?',
+  'Utilize a propriedade "color". Exemplo: color: red;'
+);
+
+criaCartao(
+  'CSS',
+  'O que faz a propriedade padding?',
+  'Define o espaço interno entre o conteúdo e a borda do elemento.'
+);
+
+criaCartao(
+  'CSS',
+  'Para que serve a propriedade display?',
+  'Controla o tipo de renderização de um elemento, como block, inline ou flex.'
+);
+
+criaCartao(
+  'CSS',
+  'Como aplicar uma classe em CSS?',
+  'Use um ponto seguido do nome da classe. Exemplo: .minhaClasse'
+);
+
+criaCartao(
+  'JavaScript',
+  'O que é JavaScript?',
+  'JavaScript é uma linguagem de programação usada para criar interatividade em páginas web.'
+);
+
+criaCartao(
+  'JavaScript',
+  'Como declarar uma variável?',
+  'Use let, const ou var. Exemplo: let nome = "Maria";'
+);
+
+criaCartao(
+  'JavaScript',
+  'O que faz a função alert()?',
+  'Exibe uma caixa de alerta no navegador com uma mensagem.'
+);
+
+criaCartao(
+  'JavaScript',
+  'Como criar uma função em JavaScript?',
+  'Use a palavra-chave function. Exemplo: function ola() { console.log("Olá"); }'
+);
+
+criaCartao(
+  'JavaScript',
+  'O que é o DOM?',
+  'DOM é a representação em árvore dos elementos HTML que o JavaScript pode manipular.'
+);
